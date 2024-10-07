f405cac5c3a01e281457a8cbd5473b4225ce5f82
Kidhurshan <d.kidhu@gmail.com>
2024-10-07 16:43:09 +0530

    Updating textField in calulator

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,6 +9,13 @@ public class Calculator implements ActionListener {
     JButton[] functionButtons;
     JButton[] numberButtons;
     JPanel panel;
+    JTextField textField;
+    //Buttons used
+    JButton addButton, subButton, mulButton, divButton;
+    JButton equButton, decButton, clearButton, backButton;
+
+    //set up the font
+    Font font = new Font("Arial",Font.BOLD, 20);
 
     //creating constructor to perform methods
     public Calculator(){
@@ -15,7 +23,17 @@ public class Calculator implements ActionListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setSize(420,550);
+        frame.setLayout(null);
 
+        //set up the text field
+        textField = new JTextField();
+        textField.setFont(font);
+        textField.setEditable(false);
+        textField.setBounds(50,25,300,50);
+
+
+
+        frame.add(textField);
         frame.setVisible(true);
     }
 
